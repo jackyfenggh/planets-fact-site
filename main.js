4c3ad83ceb64add891960e9de0dbb73a41389ce1
jackyfenggh <jackyfeng1992@gmail.com>
2022-06-14 23:03:52 +1000

    Add tab colour based on the current planet

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-// fetch planet data and set to planetData
+// fetch planet data
 
 const planet_data_url = "https://jackyfenggh.github.io/data/planet-data.json";
-// let mercury, venus, earth, mars, jupiter, saturn, uranus, neptune;
-
 let planetData = {};
-
-let pageContentSettings = {
-  'currentPlanet': 'mercury',
-  'currentTab': 'overview'
-}
 
 fetch(planet_data_url)
   .then(function (response) {
     return response = response.json();
   })
   .then(function (data) {
-    const [ mercury, venus, earth, mars, jupiter, saturn, uranus, neptune ] = data;
+    const [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune] = data;
     planetData = {
       'mercury': mercury,
       'venus': venus,
@@ -27,6 +20,49 @@ fetch(planet_data_url)
       'neptune': neptune
     };
   });
+
+// const planetColours = {
+//   'mercury': '#419EBB',
+//   'venus': '#EDA249',
+//   'earth': '#6F2ED6',
+//   'mars': '#D14C32',
+//   'jupiter': '#D83A34',
+//   'saturn': '#CD5120',
+//   'uranus': '#1EC2A4',
+//   'neptune': '#2D68F0'
+// }
+
+const planetColours = {
+  'mercury': 'mercury-colour',
+  'venus': 'venus-colour',
+  'earth': 'earth-colour',
+  'mars': 'mars-colour',
+  'jupiter': 'jupiter-colour',
+  'saturn': 'saturn-colour',
+  'uranus': 'uranus-colour',
+  'neptune': 'neptune-colour'
+}
+
+let pageContentSettings = {
+  'currentPlanet': 'mercury',
+  'currentPlanetColour': 'mercury-colour',
+  'currentTab': 'overview'
+};
+
+// const MERCURY_COLOUR = '#419EBB';
+// const VENUS_COLOUR = '#EDA249';
+// const EARTH_COLOUR = '#6F2ED6';
+// const MARS_COLOUR = '#D14C32';
+// const JUPITER_COLOUR = '#D83A34';
+// const SATURN_COLOUR = '#CD5120';
+// const URANUS_COLOUR = '#1EC2A4';
+// const NEPTUNE_COLOUR = '#2D68F0';
+
+// Helper functions
+
+function getColourName() {
+
+}
 
 function setupEventListeners() {
   const hamburgerMenu = document.getElementById('hamburger-menu');
@@ -70,10 +106,14 @@ function updatePlanet(event) {
 
   updateCurrentPlanetStyling(targetPlanet);
   updateDynamicElements(targetTab, targetPlanet);
-  updateCurrentTabStyling(targetTab);
+  updateCurrentTabStyling(targetTab, targetPlanet);
 
   pageContentSettings.currentPlanet = targetPlanet;
   pageContentSettings.currentTab = targetTab;
+
+  if (window.innerWidth <= 768) {
+    toggleMenu();
+  }
 }
 
 function updateCurrentPlanetStyling(targetPlanet) {
@@ -105,21 +145,29 @@ function updatePlanetDetailsTab(event) {
   pageContentSettings.currentTab = targetTab;
 }
 
-function updateCurrentTabStyling(targetTab) {
+function updateCurrentTabStyling(targetTab, targetPlanet = pageContentSettings.currentPlanet) {
+  // const currentPlanet = pageContentSettings.currentPlanet;
+  const currentPlanetColour = pageContentSettings.currentPlanetColour;
+
   // there are 2 navigation menus for the detail tabs
   // due to the difference in page layouts across screen sizes
 
-  // remove current-tab class from current tab elements
+  // remove current-tab and background colour class from current tab elements
   const currentTabs = document.getElementsByClassName('current-tab');
   for (let i = currentTabs.length - 1; i >= 0; i--) {
+    currentTabs[i].classList.remove(currentPlanetColour);
     currentTabs[i].classList.remove('current-tab');
   }
 
-  // add current-tab class to target tab elements
+  // add current-tab and background colour class to target tab elements
   const newCurrentTabs = document.getElementsByClassName(targetTab);
   for (let i = newCurrentTabs.length - 1; i >= 0; i--) {
+    newCurrentTabs[i].classList.add(planetColours[targetPlanet]);
     newCurrentTabs[i].classList.add('current-tab');
+    // newCurrentTabs[i].style.backgroundColor = planetColours[currentPlanet];
   }
+
+  pageContentSettings.currentPlanetColour = planetColours[targetPlanet];
 }
 
 function updateDynamicElements(targetTab, targetPlanet) {
@@ -129,7 +177,7 @@ function updateDynamicElements(targetTab, targetPlanet) {
   // update hero image
   const heroImage = document.getElementById('hero-image');
   heroImage.src = planetData[planet]['images'][details];
-  
+
   // update paragraph
   const planetDetailsParagraph = document.getElementById('info-card-text');
   planetDetailsParagraph.textContent = planetData[planet][details].content;
@@ -144,7 +192,7 @@ function updateDynamicElements(targetTab, targetPlanet) {
 
   // update planet facts
   const factFigures = document.getElementsByClassName('fact-figure');
-  const [ rotation, revolution, radius, temperature ] = factFigures;
+  const [rotation, revolution, radius, temperature] = factFigures;
 
   rotation.textContent = planetData[planet]['rotation'];
   revolution.textContent = planetData[planet]['revolution'];
